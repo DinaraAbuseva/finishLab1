df8ba5b34c38427555c312ef8d850114352952eb
DinaraAbuseva <ijk9du8@gmail.com>
2024-10-04 23:25:51 +0300

    Second commit

--- Labs/flag.py
+++ Labs/flag.py
@@ -1,11 +1,11 @@
 
-line = " "*20
+line = " " * 20
 def tailand(line, colour):
-    print(f"\x1b[48;5;{colour}m{line}\x1b[0m", end="\n")
+    print(f"\x1b[48;5;{colour}m{line}\x1b[0m", end = "\n")
 
 
-cont=[1,7,18]
-for i in cont:
-    tailand(line,i)
-for i in cont[::-1]:
-    tailand(line,i)
+cont = [1,7,18]
+for colour in cont:
+    tailand(line, colour)
+for colour in cont[::-1]:
+    tailand(line, colour)
--- Labs/pat.py
+++ Labs/pat.py
@@ -2,32 +2,40 @@
 import time
 
 
-def draw(offset,ots,length):
-    line = ' ' * length
-    otst=" " * ots
-    if ots == 0:
+def draw(offset,indent,length):
+    line = " " * length
+    indent_centre = " " * indent
+    if indent == 0:
         print(f'{" " * offset}\x1b[48;5;9m{line+" "}\x1b[0m')
-    elif ots>1:
-        print(f'{" " * (offset-ots)}\x1b[48;5;9m{line}\x1b[0m {otst+" "}\x1b[48;5;9m{line}\x1b[0m')
+    elif indent>1:
+        print(
+            f'{" " * (offset-indent)}'
+            f'\x1b[48;5;9m{line}\x1b[0m {indent_centre+" "}'
+            f'\x1b[48;5;9m{line}\x1b[0m'
+            )
     else:
-        print(f'{" " * (offset-ots)}\x1b[48;5;9m{line}\x1b[0m {otst}\x1b[48;5;9m{line}\x1b[0m')
+        print(
+            f'{" " * (offset-indent)}'
+            f'\x1b[48;5;9m{line}\x1b[0m {indent_centre}'
+            f'\x1b[48;5;9m{line}\x1b[0m'
+            )
 
 
-otstup = [0,1,2,4,8,16]
+indents = [0,1,2,4,8,16]
 def branch():
     height = 6
-    offset=20
+    offset = 20
     length = 1
     while True:
-        for ots in otstup:
-            draw(offset,ots, length)
-            if ots >= 2:
-                length = ots
-        length=1
+        for indent in indents:
+            draw(offset, indent, length)
+            if indent >= 2:
+                length = indent
+        length = 1
         print(f'\x1b[{height+2}B')
         print(f'\x1b[{offset}D')
         time.sleep(2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     branch() 
